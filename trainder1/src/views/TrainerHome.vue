d711bc78abf6f538fb2bf3e59ae050c909d09605
Filmsuphanut <nongfilm2987@gmail.com>
2021-03-13 01:18:29 +0700

    logout

--- trainder1/src/views/TrainerHome.vue
+++ trainder1/src/views/TrainerHome.vue
@@ -1,29 +1,33 @@
 <template>
 
    <v-container>
-       <Auth/>
         <h1>สวัสดี Trainer {{name}}</h1>
-
+        <v-btn @click="logout">logout</v-btn>
     </v-container>
 </template>
 
 
 <script>
-import Auth from '../views/Auth'
+//import Auth from '../views/Auth'
 
 export default {
-     name : 'index',
+     name : 't',
     data(){
         return{
             name: sessionStorage.getItem('name'),
         }
     },
     methods:{
-
+        logout(){
+            sessionStorage.removeItem('token')
+            sessionStorage.removeItem('role')
+            sessionStorage.removeItem('name')
+            this.$router.push('/')
+        }
     },
-    components: {
-        Auth,
-    }
+    // components: {
+    //     Auth,
+    // }
 
 }
 </script>
