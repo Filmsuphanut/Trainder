5ef533a90497e21ce146cd70a001726eabce1b1a
suphanut wandee <77426667+Filmsuphanut@users.noreply.github.com>
2021-04-18 14:42:08 +0700

    Update UserTable.vue

--- trainder1/src/views/UserTable.vue
+++ trainder1/src/views/UserTable.vue
@@ -9,38 +9,41 @@
     <br><br>
     <h3 align="left">ตารางออกกำลังกายของคุณ {{callname()}}</h3><br>
 
-<!-- Schedue -->
   <div>
-    <v-sheet  tile   height="54"  class="d-flex">
+    <v-sheet
+      tile
+      height="54"
+      class="d-flex"
+    >
       <v-btn
         icon
         class="ma-2"
         @click="$refs.calendar.prev()"
       >
         <v-icon>mdi-chevron-left</v-icon>
-        
       </v-btn>
 
-          <v-btn outlined class="ma-2" @click="setToday">
-            วันนี้
-          </v-btn>
+      <v-btn         
+        outlined
+        class="ma-2"
+        @click="setToday">
+        ดูกิจกรรมวันนี้
+      </v-btn>
 
-          <v-toolbar-title class="ma-3">{{title}}</v-toolbar-title>
-          <v-spacer></v-spacer>
+      <v-toolbar-title         outlined
+        class="ma-3">ฟหกฟหกฟหก</v-toolbar-title>
 
-          <v-select 
-            v-model="type"
-            :items="types"
-            dense
-            outlined
-            hide-details
-            class="ma-2"
-            label="type"
-          ></v-select>
-
+      <v-select
+        v-model="type"
+        :items="types"
+        dense
+        outlined
+        hide-details
+        class="ma-2"
+        label="type"
+      ></v-select>
 
       <v-spacer></v-spacer>
-      
       <v-btn
         icon
         class="ma-2"
@@ -48,29 +51,23 @@
       >
         <v-icon>mdi-chevron-right</v-icon>
       </v-btn>
-      
     </v-sheet>
-
     <v-sheet height="600">
       <v-calendar
-          ref="calendar"
-          v-model="value"
-          color="primary"
-          :events="events"
-          :event-color="getEventColor"
-          :event-margin-bottom="3"
-          :now="today"
-          :type="type"
-          @change="updateRange"
+        ref="calendar"
+        v-model="value"
+        color="primary"
+
+        :weekdays="weekday"
+        :type="type"
+        :events="events"
+        :event-color="getEventColor"
+        @change="updateRange"
       ></v-calendar>
     </v-sheet>
-
-
-
   </div>
-<!-- Schedue @change="getEvents"-->
 
-
+<!--@change=""-->
 </div>
   </v-container>
 </template>
@@ -79,22 +76,55 @@
 
 <script>
 
-import firebase from 'firebase'
+import firebase from "firebase";
 
   export default {
-    data: () => ({
-            today: new Date().toISOString().substr(0, 10),
+    name:"UserTable",
+    data(){
+        return{
             type: 'month',
             types: ['month', 'week', 'day'],
+            weekday: [0, 1, 2, 3, 4, 5, 6],
             value: new Date().toISOString().substr(0, 10),
+            today: new Date().toISOString().substr(0, 10),
             events: [],
             colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
             names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
-            start: null,
-            end: null,
+        }
+    },
+    methods: {
 
-    }),
+      getEventColor (event) {
+        return event.color
+      },
+      setToday () {
+        this.value = this.today;
+        this.type = 'day';
+      },
+      updateRange ({ start, end }) {
+        // You could load events from an outside source (like database) now that we have the start and end dates on the calendar
+        this.start = start
+        this.end = end
+      },
 
+
+
+
+    callname(){
+        let user = firebase.auth().currentUser
+        let displayname
+
+        if (user != null) {
+            displayname = user.displayName
+        return displayname
+        }
+    },
+    back() {
+        let previous = this.$store.state.previous.pre;
+        this.$router.push(previous);
+    },
+
+    },
     computed: {
       title () {
         const { start, end } = this
@@ -130,53 +160,8 @@ import firebase from 'firebase'
         })
       },
     },
-
-
-
     mounted () {
       this.$refs.calendar.checkChange()
-    },
-    
-    methods: {
-      getEventColor (event) {
-        return event.color
-      },
-      setToday () {
-        this.value = this.today
-      },
-      prev () {
-        this.$refs.calendar.prev()
-      },
-      next () {
-        this.$refs.calendar.next()
-      },
-
-
-      updateRange ({ start, end }) {
-        // You could load events from an outside source (like database) now that we have the start and end dates on the calendar
-        this.start = start
-        this.end = end
-      },
-      nth (d) {
-        return d > 3 && d < 21
-          ? 'th'
-          : ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'][d % 10]
-      },
-
-      callname(){
-          let user = firebase.auth().currentUser
-          let displayname
-
-          if (user != null) {
-              displayname = user.displayName
-          return displayname
-          }
-      },
-      back() {
-          let previous = this.$store.state.previous.pre;
-          this.$router.push(previous);
-      },
-
     },
 
 
