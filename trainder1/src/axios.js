bf12f9c95a0d2f646c1a141c8860edd280a73cb1
Glairly <62010787@kmitl.ac.th>
2021-05-04 13:56:55 +0700

    fix cors

--- trainder1/src/axios.js
+++ trainder1/src/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 
-axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*"
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = true
 axios.defaults.baseURL = process.env.VUE_APP_ENDPOINT
